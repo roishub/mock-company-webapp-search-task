8233af3ec93075c0d3ec9a8a6d00f20997e4cc28
ROIS <130916596+roishub@users.noreply.github.com>
2024-03-11 13:53:19 +0530

    Update SearchService.java

--- mock-company-webapp-search-task/src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ mock-company-webapp-search-task/src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -6,8 +6,7 @@ import com.mockcompany.webapp.model.ProductItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,33 +22,7 @@ public class SearchService {
         return (int) productItemRepository.count();
     }
 
-    public Collection<ProductItem> searchProducts(String query) {
-        Iterable<ProductItem> allItems = productItemRepository.findAll();
-        List<ProductItem> itemList = new ArrayList<>();
-
-        boolean exactMatch = false;
-        if (query.startsWith("\"") && query.endsWith("\"")) {
-            exactMatch = true;
-            query = query.substring(1, query.length() - 1);
-        } else {
-            query = query.toLowerCase();
-        }
-
-        for (ProductItem item : allItems) {
-            boolean nameMatches;
-            boolean descMatches;
-            if (exactMatch) {
-                nameMatches = query.equals(item.getName());
-                descMatches = query.equals(item.getDescription());
-            } else {
-                nameMatches = item.getName().toLowerCase().contains(query);
-                descMatches = item.getDescription().toLowerCase().contains(query);
-            }
-            if (nameMatches || descMatches) {
-                itemList.add(item);
-            }
-        }
-
-        return itemList;
+    public List<ProductItem> search() {
+        return Collections.emptyList();
     }
 }
